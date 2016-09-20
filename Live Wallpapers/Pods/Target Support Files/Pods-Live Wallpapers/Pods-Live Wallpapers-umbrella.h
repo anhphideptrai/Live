4856b1e835db1ed6116a3bb3186c8fb28c90c54b
Phi Nguyen <phi.nguyen@bstarsolutions.com>
2016-09-20 14:17:24 +0700

    - Changed proj

--- Live Wallpapers/Pods/Target Support Files/Pods-Live Wallpapers/Pods-Live Wallpapers-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Live_WallpapersVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Live_WallpapersVersionString[];
-
--- README.md
+++
@@ -1,3 +0,0 @@
-# Live
-Live Wallpapers
-Test Push code
